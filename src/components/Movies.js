4a95350cdf4cae18db0dd1ffc182fe8a7ad0ff27
+ <+>
2023-04-29 08:16:09 +0300

    completed react-router-routes lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <ul>
+        {movies.map((movie, index) => (
+          <li key={index}>
+            {movie.title} - {movie.time} minutes
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {movies.reduce(
+          (allGenres, movie) => [...allGenres, ...movie.genres],
+          []
+        ).map((genre, index) => (
+          <li key={index}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Movies;
